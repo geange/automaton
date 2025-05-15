524457e188a4c825cbbc5807c85806b2f5d22aeb
geange <geange@outlook.com>
2025-05-16 00:13:21 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/geange/automaton
+
+go 1.24.3
+
+require github.com/bits-and-blooms/bitset v1.22.0
---
+++ map.go
@@ -0,0 +1,71 @@
+package automaton
+
+type Hash interface {
+	Hash() int64
+}
+
+type Map[K Hash, V any] struct {
+	mp     map[int64]int
+	values []*MapEntry[K, V]
+	rmIdx  []int
+}
+
+func NewMap[K Hash, V any]() *Map[K, V] {
+	return &Map[K, V]{
+		mp:     map[int64]int{},
+		values: make([]*MapEntry[K, V], 0),
+		rmIdx:  make([]int, 0),
+	}
+}
+
+func (m *Map[K, V]) Put(key K, value V) {
+	code := key.Hash()
+	idx, ok := m.mp[code]
+	if ok {
+		m.values[idx].Key = key
+		m.values[idx].Value = value
+		return
+	}
+	if len(m.rmIdx) == 0 {
+		m.values = append(m.values, &MapEntry[K, V]{Key: key, Value: value})
+		m.mp[code] = len(m.values) - 1
+		return
+	}
+	idx = m.rmIdx[len(m.rmIdx)-1]
+	m.mp[code] = idx
+	m.values[idx].Key = key
+	m.values[idx].Value = value
+	m.rmIdx = m.rmIdx[:len(m.rmIdx)-1]
+}
+
+func (m *Map[K, V]) Get(key K) (v V, ok bool) {
+	code := key.Hash()
+	idx, ok := m.mp[code]
+	if ok {
+		return m.values[idx].Value, true
+	}
+	return
+}
+
+func (m *Map[K, V]) Remove(key K) bool {
+	code := key.Hash()
+	idx, ok := m.mp[code]
+	if !ok {
+		return false
+	}
+
+	delete(m.mp, code)
+	m.rmIdx = append(m.rmIdx, idx)
+	return true
+}
+
+func (m *Map[K, V]) Clear() {
+	m.values = m.values[:0]
+	m.rmIdx = m.rmIdx[:0]
+	clear(m.mp)
+}
+
+type MapEntry[K Hash, V any] struct {
+	Key   K
+	Value V
+}
